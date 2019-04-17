3faf32b784e279023a30fcb97ca2db5974d87ef6
Michael Nash <mikenash1990@gmail.com>
2019-04-17 16:48:48 +0100

    Set up skeleton action names

--- src/ExpensesSmartContract.cpp
+++ src/ExpensesSmartContract.cpp
@@ -1,5 +1,54 @@
 #include <ExpensesSmartContract.hpp>
-ACTION ExpensesSmartContract::hi( name nm ) {
+ACTION ExpensesSmartContract::hi( name nm )
+{
    /* fill in action body */
    print_f("Name : %\n",nm);
+}
+
+class [[eosio::contract]] employee_table : public contract
+{
+  public:
+      using contract::contract;
+      
+      [[eosio::action]]
+      void addemp( account_name const user, string employee_name )
+      {
+        
+      }
+      
+      [[eosio::action]]
+      void setsal( account_name const user, string employee_name, uint64_t salary)
+      {
+        
+      }
+      
+      [[eosio::action]]
+      void delemp( account_name const user, string employee_name)
+      {
+        
+      }
+      
+      [[eosio::action]]
+      void claimexp( account_name const user, string employee_name, uint64_t expenses)
+      {
+        
+      }
+      
+      [[eosio::action]]
+      void appexp( account_name const user)
+      {
+        
+      }
+      
+      [[eosio::action]]
+      void rejexp( account_name const user)
+      {
+        
+      }
+      
+      [[eosio::action]]
+      void paysal( account_name const user)
+      {
+        
+      }
 }
